ff37d856f9a5d45ae53b88778f594dab73ba2743
ahmedelamin-hub <ahmedelamin42@gmail.com>
2023-10-05 13:57:24 -0700

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,14 @@
-int main (void)
+#include <stdio.h>
+
+/**
+ * main - this is main function 
+ * 
+ * Return: always 0
+ */
+
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-		return (0);
+	return (0);
 }
 
