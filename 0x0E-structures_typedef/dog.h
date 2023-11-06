407b8fb2f3e5e31e06b75bf01d5c5022fa482c0c
ahmedelamin-hub <ahmedelamin42@gmail.com>
2023-11-06 08:10:59 -0800

    question 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - function that lists dogs
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
