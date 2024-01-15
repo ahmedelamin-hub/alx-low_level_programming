e3091da060a8890f0fa0d1d363095e88930b5e3c
root <ahmedelamin42@gmail.com>
2024-01-15 09:12:37 -0800

    L

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,4 @@
 #!/bin/bashi
-gcc -Wall -pedantic -Werror -Wextra -c -fPIC *.C
+gcc -Wall -pedantic -Werror -Wextra -c -fPIC *.c
 gcc -shared -o liball.so *.o
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
